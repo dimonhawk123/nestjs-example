e90b89948f8e7fa849ac824ea624583ce217e8e6
Dmitrii Ryzhkov <53188466+dimonhawk123@users.noreply.github.com>
2023-06-29 11:35:50 +0500

    Initialize part-71

---
+++ src/emailConfirmation/emailConfirmation.module.ts
@@ -0,0 +1,30 @@
+import { Module } from '@nestjs/common';
+import { EmailConfirmationService } from './emailConfirmation.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { EmailModule } from '../email/email.module';
+import { JwtModule } from '@nestjs/jwt';
+import { EmailConfirmationController } from './emailConfirmation.controller';
+import { UsersModule } from '../users/users.module';
+import { FeatureFlagsModule } from '../featureFlags/featureFlags.module';
+
+@Module({
+  imports: [
+    ConfigModule,
+    EmailModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        service: configService.get('EMAIL_SERVICE'),
+        user: configService.get('EMAIL_USER'),
+        password: configService.get('EMAIL_PASSWORD'),
+      }),
+    }),
+    JwtModule.register({}),
+    UsersModule,
+    FeatureFlagsModule,
+  ],
+  providers: [EmailConfirmationService],
+  exports: [EmailConfirmationService],
+  controllers: [EmailConfirmationController],
+})
+export class EmailConfirmationModule {}
